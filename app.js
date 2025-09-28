e480b0e0594775536bc5098d26dd4aff5994d0df
TaherPro <alfakihtaher@gmail.com>
2025-09-28 10:36:39 -0400

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const gallery = document.getElementById("gallery");
+const modal = document.getElementById("movie-details");
+const movieInfo = document.getElementById("movie-info");
+const closeBtn = document.getElementById("close-modal");
+
